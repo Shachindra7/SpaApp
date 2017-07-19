0c5495d8ff14669f98b4478b346ad0612c0a03d8
Shachindra7 <sachindra.tripathi@jktech.com>
2017-07-19 16:30:46 +0530

    category model class

---
+++ app/src/main/java/com/example/shachindratripathi/spaapp/Category.java
@@ -0,0 +1,61 @@
+package com.example.shachindratripathi.spaapp;
+
+/**
+ * Created by shachindratripathi on 19/7/17.
+ */
+
+public class Category
+{
+    private String category_id;
+    private String city_id;
+    private String name;
+    private String image;
+    private String edate;
+
+    public String getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(String category_id) {
+        this.category_id = category_id;
+    }
+
+    public String getCity_id() {
+        return city_id;
+    }
+
+    public void setCity_id(String city_id) {
+        this.city_id = city_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getEdate() {
+        return edate;
+    }
+
+    public void setEdate(String edate) {
+        this.edate = edate;
+    }
+
+    public Category() {
+
+
+
+}
+}
+
