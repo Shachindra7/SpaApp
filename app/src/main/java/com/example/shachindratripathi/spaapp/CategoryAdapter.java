97bb9227ad62fca22faf659095b27a1447bd655b
Shachindra7 <sachindra.tripathi@jktech.com>
2017-07-20 13:05:35 +0530

    Modified by Shachindra

--- app/src/main/java/com/example/shachindratripathi/spaapp/CategoryAdapter.java
+++ app/src/main/java/com/example/shachindratripathi/spaapp/CategoryAdapter.java
@@ -4,13 +4,17 @@ import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static android.R.attr.resource;
 
 /**
  * Created by shachindratripathi on 19/7/17.
@@ -19,14 +23,47 @@ import java.util.List;
 public class CategoryAdapter extends ArrayAdapter<Category> {
 
 
-    public CategoryAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Category> objects) {
+
+    ArrayList<Category> ArrayListCategory;
+    int Resource;
+    Context context;
+    LayoutInflater vi;
+
+
+    public CategoryAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull ArrayList<Category> objects) {
         super(context, resource, objects);
+
+        ArrayListCategory = objects;
+        Resource = resource;
+        this.context = context;
+        vi = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        ViewHolder holder;
+        if (convertView == null){
+            convertView= vi.inflate(Resource,null);
+            holder = new ViewHolder();
+            holder.imageview = (ImageView)convertView.findViewById(R.id.imageView);
+            holder.tvcategoryId = (TextView)convertView.findViewById(R.id.tvcategoryId);
+            holder.tvname = (TextView)convertView.findViewById(R.id.tvcityId);
+            holder.tvname = (TextView)convertView.findViewById(R.id.tvname);
+            holder.tvname = (TextView)convertView.findViewById(R.id.tvimage);
+            holder.tvname = (TextView)convertView.findViewById(R.id.tvedate);
+            holder.tvname = (TextView)convertView.findViewById(R.id.tvServices);
+
+            convertView.setTag(holder); 
+
+
+        }
+        else {
+            holder = (ViewHolder)convertView.getTag();
+
+        }
+       return convertView ;
     }
 
 }
--- app/src/main/java/com/example/shachindratripathi/spaapp/MainActivity.java
+++ app/src/main/java/com/example/shachindratripathi/spaapp/MainActivity.java
@@ -17,7 +17,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
-import static android.R.attr.data;
 import static android.R.attr.text;
 
 
@@ -33,23 +32,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.json);
         MainActivity m = new MainActivity();
-         readJson(new View(this));
-
+        readJson(new View(this));
         categoryList = new ArrayList<>();
 
-
-
     }
-
-
     public String readJson(View view) {
-        TextView JSONtextView = (TextView) findViewById(R.id.JSONtextView);
+        ListView JSONtextView = (ListView) findViewById(R.id.JSONtextView);
 
 
         AssetManager am = getAssets();
         String text = "";
         try {
-            InputStream is = getAssets().open("spa_dummy.json");
+            InputStream is = getAssets().open("spa_dummy.txt");
             int size = is.available();
             byte[] buffer = new byte[size];
             is.read(buffer);
@@ -59,8 +53,6 @@ public class MainActivity extends AppCompatActivity {
             ex.printStackTrace();
         }
         return text;
-
-
     }
 
     private void parseJson(String s) {
@@ -68,15 +60,22 @@ public class MainActivity extends AppCompatActivity {
         StringBuilder builder = new StringBuilder();
         try {
             JSONObject jsonObject = new JSONObject(String.valueOf(text));
-            JSONArray jsonArray = new JSONArray("data");
-
-
-
+            JSONArray jsonArray = jsonObject.getJSONArray("data");
+            for(int i = 0; i<= jsonArray.length(); i++){
+                Category category = new Category();
+                JSONObject realObject = jsonArray.getJSONObject(i);
+                category.setCategory_id(realObject.getString("category_id"));
+                category.setCity_id(realObject.getString("category_id"));
+                category.setName(realObject.getString("category_id"));
+                category.setImage(realObject.getString("category_id"));
+                category.setEdate(realObject.getString("category_id"));
+                categoryList.add(category);
+            }
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
-
+        CategoryAdapter adapter = new CategoryAdapter(getApplicationContext(),R.layout.category,categoryList);
+        list.setAdapter(adapter);
     }
 }
 
