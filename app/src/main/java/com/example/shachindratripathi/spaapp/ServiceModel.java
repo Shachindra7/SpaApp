53b74f89204de74c9ec5e21e4963925fd1c73a9d
root <root@saurabh-OptiPlex-3010.(none)>
2017-07-25 17:50:05 +0530

    models and parsing Json

---
+++ app/src/main/java/com/example/shachindratripathi/spaapp/ServiceModel.java
@@ -0,0 +1,94 @@
+package com.example.shachindratripathi.spaapp;
+
+/**
+ * Created by shachindratripathi on 25/7/17.
+ */
+
+
+
+      class ServiceModel  {
+        private String id;
+        private String category_id;
+        private String city_id;
+        private String title;
+        private String description;
+        private String duration;
+        private String price;
+        private String image;
+        private String edate;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getCategory_id() {
+            return category_id;
+        }
+
+        public void setCategory_id(String category_id) {
+            this.category_id = category_id;
+        }
+
+        public String getCity_id() {
+            return city_id;
+        }
+
+        public void setCity_id(String city_id) {
+            this.city_id = city_id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getDuration() {
+            return duration;
+        }
+
+        public void setDuration(String duration) {
+            this.duration = duration;
+        }
+
+        public String getPrice() {
+            return price;
+        }
+
+        public void setPrice(String price) {
+            this.price = price;
+        }
+
+        public String getImage() {
+            return image;
+        }
+
+        public void setImage(String image) {
+            this.image = image;
+        }
+
+        public String getEdate() {
+            return edate;
+        }
+
+        public void setEdate(String edate) {
+            this.edate = edate;
+        }
+    }
+
+
+
